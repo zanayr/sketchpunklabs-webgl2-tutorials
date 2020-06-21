3118d5e2a8a9971c60016d86b5d5fd91cdd28575
Ryan Fickenscher <rsficken@gmail.com>
2020-06-20 19:02:20 -0700

    finished lesson 003

---
+++ 003/attributes.js
@@ -0,0 +1,7 @@
+export const POSITION_LOCATION = 0;
+export const NORMAL_LOCATION = 1;
+export const UV_LOCATION = 2;
+
+export const POSITION_NAME = 'a_position';
+export const NORMAL_NAME = 'a_normal';
+export const UV_NAME = 'a_uv';
